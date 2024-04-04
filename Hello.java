ceeb2356d09d9036c590784adbb17248e64c644c
Keshav <s.keshav13142@gmail.com>
2024-04-04 12:27:23 +0530

    add sum feature

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
- class Hello {
-public static void main(String[] args) {
-  System.out.println("New version");
-}
+class Hello {
+  public static void main(String[] args) {
+    System.out.println(add(10,20));
+  }
+  public static int add(int a , int b){
+    return a+b;
+  }
 }
